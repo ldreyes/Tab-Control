42fbd90e21afd839e0abe9af46adfc1b45a192e4
Julie Xue <julie.xue11@gmail.com>
2015-09-29 20:57:11 -0400

    trim tab names longer than 20

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,17 @@
+function strimString(string, length){
+  if(string.length > length) {
+    return string.substring(0,length-1)+"...";
+  }
+  return string;
+}
+
+
 function getTabs(queryInfo, callback){
   chrome.tabs.query(queryInfo, function (tabs){
 
     for (index in tabs){
       console.log(tabs[index].title);
-      document.getElementById("list").innerHTML += ("* " + tabs[index].title + "<br>");
+      document.getElementById("list").innerHTML += ("* " + strimString(tabs[index].title, 20) + "<br>");
     }
     callback();
   });
