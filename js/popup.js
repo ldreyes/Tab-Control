e0d83b5c9b069148b6a6ba2b0b6c5b87850aae5a
Leslie Reyes <diane@Manolito-Reyess-iMac.local>
2015-09-30 23:47:08 -0400

    changed font styling, added highlight feature and ability to navigate to clicked tab

--- js/popup.js
+++ js/popup.js
@@ -9,9 +9,14 @@ function strimString(string, length){
 function getTabs(queryInfo, callback){
   chrome.tabs.query(queryInfo, function (tabs){
 
+    var list = document.getElementById('list');
     for (index in tabs){
       console.log(tabs[index].title);
-      document.getElementById("list").innerHTML += ("* " + strimString(tabs[index].title, 20) + "<br>");
+
+      var listElement = document.createElement('li');
+      listElement.appendChild(document.createTextNode(strimString(tabs[index].title, 20)));
+      listElement.setAttribute('id', index);
+      list.appendChild(listElement);
     }
     callback();
   });
@@ -21,4 +26,11 @@ document.addEventListener('DOMContentLoaded', function(){
   getTabs({}, function(){
     console.log("All tabs loaded");
   });
+});
+
+//Navigates to the tab that the user clicks
+//TODO: fix to work when tabs are in another window
+var l = document.getElementById('list');
+l.addEventListener('click', function(event){
+  chrome.tabs.highlight({ 'tabs' : parseInt(event.target.id) }, function(window){});
 });
